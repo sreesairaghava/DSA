2609395936c9d77785662862b57b2a586a660860
Sree Sai Raghava <raghava.rym@gmail.com>
2021-09-07 11:00:55 +0530

    Implemented Arrays: largest Element

--- Arrays/LargestElement.cpp
+++ Arrays/LargestElement.cpp
@@ -3,9 +3,28 @@
 
 using namespace std;
 
+//Function to getLargestElement
+int getLargestElement(int arr[], int size)
+{
+    int res = 0;
+    for (int i = 1; i < size; i++)
+    {
+        if (arr[i] > arr[res])
+        {
+            res = i;
+        }
+    }
+    return res;
+}
+
 //Main
 int main()
 {
     //Init Array
+    int arr[] = {300, 5, 60, 7, 21};
+    std::cout << "Largest" << std::endl;
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int largest = getLargestElement(arr, n);
+    cout << arr[largest] << endl;
     return 0;
 }
